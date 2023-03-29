577e17b2e04082fedfacdcc9f69f4b139453c9e4
Ed Chalstrey <edwardchalstrey@gmail.com>
2023-03-29 11:40:03 +0100

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Standard Architecture for Trusted Research Environments'
+copyright = ''
+author = ''
 
-release = '0.1'
-version = '0.1.0'
+release = '0.0'
+version = '0.0.0'
 
 # -- General configuration
 
